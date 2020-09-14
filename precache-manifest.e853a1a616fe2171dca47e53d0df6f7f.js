6a004148760969506c35946b78db063ec4c63fc7
praveenkalady <praveenkvinfo@gmail.com>
2020-09-14 13:04:18 +0530

    Updates

---
+++ precache-manifest.e853a1a616fe2171dca47e53d0df6f7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a11cfe1ac4f34124862f9c738715db",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "dfbe8876737cc71f1f2c",
+    "url": "/contactmanager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6b5c1730914487b4576a",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "dfbe8876737cc71f1f2c",
+    "url": "/contactmanager/static/js/2.25145498.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.25145498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5c1730914487b4576a",
+    "url": "/contactmanager/static/js/main.56d77554.chunk.js"
+  },
+  {
+    "revision": "e155b645805d8202c894",
+    "url": "/contactmanager/static/js/runtime-main.8912a38d.js"
+  }
+]);
